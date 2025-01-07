9201d8ac862e7e051cacf7ff5f8625193b59f458
Pablo Garcia Barros <pgarciabarros86@gmail.com>
2025-01-07 15:00:19 +0100

    Muestra en pantalla una variable

---
+++ TiposDatos/Ejercicio1/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Integer variable;
+        variable = 14788;
+
+        System.out.println("La variable tiene valor de: " + variable);    
+    }
+}
