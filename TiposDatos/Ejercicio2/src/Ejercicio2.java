148dd66ad51ff0ff30e4eb7acdf552f38299ceeb
Pablo Garcia Barros <pgarciabarros86@gmail.com>
2025-01-07 15:05:33 +0100

    Muestra en pantalla variable que cambia de valor

---
+++ TiposDatos/Ejercicio2/src/Ejercicio2.java
@@ -0,0 +1,12 @@
+public class Ejercicio2 {
+    public static void main(String[] args) throws Exception {
+
+        Integer variable = 2;
+
+        System.out.println("El valor de variable es: " + variable);
+
+        variable = 5;
+
+        System.out.println("El nuevo valor de variable es: " + variable);
+    }
+}
