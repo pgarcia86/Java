54bc5f6094a20403e6420df476bab45b5741b1bf
Pablo Garcia Barros <pgarciabarros86@gmail.com>
2025-01-16 19:47:42 +0100

    Ejercicios basicos de manipulacion de objetos

--- Objetos/ObjetosBasicos/src/ObjetosBasicos.java
+++ Objetos/ObjetosBasicos/src/ObjetosBasicos.java
@@ -3,10 +3,14 @@ public class ObjetosBasicos {
         Car auto = new Car("Ford", "Mondeo", 1990, "Rojo");
         Persona persona = new Persona("Pablo", 38);
         Rectangulo rectangulo = new Rectangulo(15.2, 7.8);
+        CuentaBancaria cuentaBancaria = new CuentaBancaria("Pablo Garcia", 1700);
 
         System.out.println(auto.toString());
         persona.saludar();
         System.out.printf("El area del rectangulo es: " + "%1.3f\n",rectangulo.area());
         System.out.println("El perimetro del rectangulo es: " + rectangulo.perimetro());
+        cuentaBancaria.depositar(300);
+        cuentaBancaria.retirar(700);
+        System.out.println(cuentaBancaria.toString());
     }
 }
