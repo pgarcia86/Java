513b38fa6a3ecbd62c94c56e08cfa4138b136dea
Pablo Garcia Barros <pgarciabarros86@gmail.com>
2025-01-16 20:25:23 +0100

    Ejercicios intermedios de manipulacion de objetos

--- Objetos/ObjetosIntermedio/src/ObjetosIntermedios.java
+++ Objetos/ObjetosIntermedio/src/ObjetosIntermedios.java
@@ -1,6 +1,28 @@
+import java.util.ArrayList;
+
 public class ObjetosIntermedios {
     public static void main(String[] args) throws Exception {
 
-        
+        double precioFinal = 0.0;
+        ArrayList<Producto> productos = new ArrayList<Producto>();
+        Estudiante estudiante = new Estudiante("Pablo Garcia", 38, 5.9, 4.7,8.0);
+        Empleado empleado = new Empleado("Pablo Garcia", 1435);
+        Empleado gerente = new Gerente("Pablo Garcia Barros", 1435, 800);
+
+        productos.add(new Producto("Agua", 1.5, 50));
+        productos.add(new Producto("Leche", 1.5, 40));
+        productos.add(new Producto("Vino", 3, 30));
+        productos.add(new Producto("Cerveza", 2.50, 20));
+        productos.add(new Producto("Agua con gas", 2.5, 10));
+
+        for(int i = 0; i < productos.size(); i++){
+            precioFinal += productos.get(i).totalValor();
+        }
+
+        System.out.println("El valor total de todos los productos es: " + precioFinal);
+        System.out.println(estudiante.toString());
+        System.out.println(empleado.mostrarInfo());
+        System.out.println(gerente.mostrarInfo());
+
     }
 }
